fa90ef869a0ba8a104b9bf2dc69a8c060b579073
muskanrathod <70010572+muskanrathod@users.noreply.github.com>
2022-09-19 13:09:40 +0530

    Update CisDateJava.java

--- cisdate/src/main/java/com/cis/cisdate/CisDateJava.java
+++ cisdate/src/main/java/com/cis/cisdate/CisDateJava.java
@@ -5,9 +5,9 @@ import android.widget.Toast;
 
 public class CisDateJava {
 
-    public void myDate(Context ctx, String msg) {
+    public static void myDate(Context ctx, String msg) {
         Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
     }
-
+  
 
 }
